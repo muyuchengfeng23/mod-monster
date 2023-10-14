58c1005b61cde277f8d11c66eaa8eaaffb85f7ba
muyuchengfeng23 <984890126@qq.com>
2023-10-14 19:25:41 +0800

    add mod infos

---
+++ modinfo.lua
@@ -0,0 +1,19 @@
+name = "鸽门永存"
+description = "其实说鸽就鸽, 也是一种不鸽"
+author = "Little monster"
+version = "Alpha 0.0.1"
+forumthread = ""
+api_version = 10
+
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+all_clients_require_mod = true
+
+icon_atlas = ""  --mod图标文件.xml
+icon = "" --mod图标文件.tex
+
+server_filter_tags = {
+"character",
+"item",
+} --服务器mod标签
